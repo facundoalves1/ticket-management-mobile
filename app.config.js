44f45477584422608b9d069053378fb721e1df5e
rosendoalves <rosendoalves05@gmail.com>
2024-01-04 20:51:16 -0300

    build2

--- app.config.js
+++ app.config.js
@@ -4,5 +4,11 @@ export default ({ config }) => ({
   ...config,
   extra: {
     baseUrl: process.env.BASE_URL,
+    eas: {
+      projectId: "9cf51dfb-561f-45f5-bde9-9b21231973b1"
+    }
   },
+  android: {
+    package: "com.rosendoalves.ticket_management_mobile"
+  }
 });
