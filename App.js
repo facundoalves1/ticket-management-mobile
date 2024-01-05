b4fe948b26288ae15c05f3c4bf68245746edd7ad
Rosendo Alves <rosendoalves05@gmail.com>
2024-01-05 02:06:15 -0300

    build ok

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Main from "./src/Main";
+import 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return <Main/>;
--- app.config.js
+++ app.config.js
@@ -3,12 +3,13 @@ import "dotenv/config";
 export default ({ config }) => ({
   ...config,
   extra: {
-    baseUrl: process.env.BASE_URL,
+    baseUrl: process.env.EXPO_PUBLIC_API_URL,
     eas: {
       projectId: "9cf51dfb-561f-45f5-bde9-9b21231973b1"
     }
   },
   android: {
-    package: "com.rosendoalves.ticket_management_mobile"
+    package: "com.rosendoalves.ticket_management_mobile",
+    packageType: "APK",
   }
 });
