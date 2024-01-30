3fc976fecf58969e105af805188b997e3caa0f7c
rosendoalves <rosendoalves05@gmail.com>
2024-01-30 01:25:16 -0300

    filtros en history tickets

--- src/pages/HistoryScreen.jsx
+++ src/pages/HistoryScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -10,30 +10,141 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import TicketCard from "../components/TicketCard";
 import { GET_TICKETS_BY_USERS } from "../api/ticketApi";
 import { useFocusEffect } from "@react-navigation/core";
+import { useLogin } from "../context/LoginProvider";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function HistoryScreen({ navigation }) {
   const [expandedCardIndex, setExpandedCardIndex] = useState(null);
   const [tickets, setTickets] = useState([]);
   const [visible, setVisible] = useState(false);
   const [isError, setError] = useState(false);
+  const { setLogedIn } = useLogin()
   const toggleDetailCard = (index) => {
     setExpandedCardIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
+  const timeOptions = [
+    { label: "Hoy", value: 1 },
+    { label: "Semana", value: 2 },
+    { label: "Mes", value: 3 },
+    { label: "Todo", value: 4 },
+  ]
+
+  const [filteredTickets, setFilteredTickets] = useState([]);
+
+  const handleFilterTime = (periodity) => {
+    const currentDate = new Date();
+    let filteredTickets = [];
+
+    switch (periodity) {
+      case 1:
+        filteredTickets = tickets.filter(ticket => {
+          const ticketDate = new Date(ticket.createdAt);
+          const ticketDateFormatted = new Date(ticketDate.getFullYear(), ticketDate.getMonth(), ticketDate.getDate());
+          const currentDateFormatted = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+          return ticketDateFormatted.getTime() === currentDateFormatted.getTime();
+        });
+        break;
+      case 2:
+        const firstDayOfWeek = new Date(currentDate);
+        firstDayOfWeek.setDate(currentDate.getDate() - currentDate.getDay());
+        const lastDayOfWeek = new Date(currentDate);
+        lastDayOfWeek.setDate(currentDate.getDate() - currentDate.getDay() + 6);
+
+        filteredTickets = tickets.filter(ticket => {
+          const ticketDate = new Date(ticket.createdAt);
+          return ticketDate >= firstDayOfWeek && ticketDate <= lastDayOfWeek;
+        });
+        break;
+      case 3:
+        const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+        filteredTickets = tickets.filter(ticket => {
+          const ticketDate = new Date(ticket.createdAt);
+          return ticketDate >= firstDayOfMonth && ticketDate <= lastDayOfMonth;
+        });
+        break;
+      case 4:
+        filteredTickets = tickets;
+        break;
+      default:
+        break;
+    }
+    setFilteredTickets(filteredTickets);
+    setExpandedCardIndex(null);
+  };
+
+  useEffect(() => {
+    setFilteredTickets(tickets);
+  }, [tickets]);
+
+
   useFocusEffect(
     useCallback(() => {
-      GET_TICKETS_BY_USERS().then((res) => setTickets(res));
+      GET_TICKETS_BY_USERS().then((res) => {
+        if (res.length > 0) {
+          res.sort((a, b) => {
+            const dateA = new Date(a.createdAt);
+            const dateB = new Date(b.createdAt);
+            return dateB - dateA;
+          });
+          setTickets(res)
+        }
+      })
+        .catch(err => {
+          const { message } = err.response.data;
+          if (message == "PAYLOAD_DATA_NOT_FOUND") {
+            AsyncStorage.removeItem("token");
+            setLogedIn(null);
+          }
+        });
     }, [])
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{flex:1}}>
+      <View style={{ flex: 1 }}>
         <Text style={styles.title}>Historial de Tickets</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            marginBottom: 5
+
+          }}
+        >
+          {timeOptions.map(option => {
+            return (
+              <TouchableOpacity
+                onPress={() => handleFilterTime(option.value)}
+                key={option.label}
+              >
+                <LinearGradient
+                  colors={["#E6C84F", "#E8807F"]}
+                  style={{
+                    height: 50,
+                    width: 90,
+                    borderRadius: 5,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: 3,
+                  }}
+                >
+                  <Text>
+                    {option.label}
+                  </Text>
+                </LinearGradient>
+              </TouchableOpacity>
+            )
+          })}
+        </View>
         {tickets.length > 0 ? (
           <FlatList
             keyExtractor={(item) => item._id}
-            data={tickets}
+            data={filteredTickets}
             renderItem={({ item, index }) => (
               <TouchableOpacity onPress={() => toggleDetailCard(index)}>
                 <TicketCard
@@ -49,7 +160,7 @@ export default function HistoryScreen({ navigation }) {
             )}
           />
         ) : (
-          <Text style={{...styles.item, textAlign: 'center'}}>No hay tickets creados</Text>
+          <Text style={{ ...styles.item, textAlign: 'center' }}>No hay tickets creados</Text>
         )}
       </View>
     </SafeAreaView>
